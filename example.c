0beecf4da308b68ad1364ea0b4e543a6fdd035f9
marim <marimat2014@gmail.com>
2020-05-30 21:07:05 +0200

    example for 3 elements

---
+++ example.c
@@ -0,0 +1,27 @@
+//
+// Created by marim on 30.05.2020.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    char set[] = {'a','b','c'}; //задана множина
+    int n = 3; //кількість елементів
+
+    int i, j, size;
+
+    size = pow(2, n); //для підрахунку кількості підмножин використовється формула 2^n
+    for ( i = 0; i < size; i++ )
+    {
+        printf("{");
+        for ( j = 0; j < size; j++ ) //перебір
+            if ( i & (1 << j)  //логічний сдвиг вліво, & - и
+                printf("%c", set[j]); //вивід j елементу з set
+        printf("}\n");
+    }
+    return 0;
+}
+
+
